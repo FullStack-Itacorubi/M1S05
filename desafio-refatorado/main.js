e2a4301ff0104ccbc55a26184ef4b1eafe6e4b43
Kelvis Borges <kelvisbcc@gmail.com>
2023-03-03 22:38:58 -0300

    feat(aula4): desafio com modulos exemplificando dev tools

--- desafio-refatorado/main.js
+++ desafio-refatorado/main.js
@@ -1,14 +1,20 @@
 import { somador } from './src/modules/calculos.js';
-import { carros, pesquisarCarros } from './src/modules/carros.js';
+import { pesquisarCarros } from './src/modules/carros.js';
 import { Ingresso } from './src/modules/classes/Ingresso.js';
 import { comprarIngresso, venderIngresso } from './src/modules/ingressos.js';
 import { pesquisarTermo } from './src/modules/utils.js';
 
+
 // INGRESSO
 const ingresso1 = new Ingresso('Coldplay', 400.00, '28/08/2023', '12SADV1');
 const ingresso2 = new Ingresso('U2', 350.00, '04/09/2023', 'L341AV13');
+
 const ingressoComprado1 = comprarIngresso(ingresso1, 'Ana');
 const ingressoComprado2 = comprarIngresso(ingresso2, 'Joaquim');
+
+ingresso1.valor = 500;
+ingresso1.valor = 1000;
+ingresso1.valor = 1400;
 const ingressoVendido = venderIngresso(ingresso1, 'Joaquim');
 
 console.log(ingressoComprado1);
@@ -25,4 +31,29 @@ const termo = 'VsiDA';
 console.log(pesquisarTermo(frase, termo) ? 'Encontrou o termo' : 'Não encontrou o termo');
 
 // BUSCAR CARROS
-console.log('Carros encontrados são: ', pesquisarCarros(carros, 'renault'));
+const termoCarro = 'FIAT';
+
+var CARROS_GLOBAL = [
+    {
+        nome: 'Fiat - Uno - 2012',
+    },
+    {
+        nome: 'Volkswagen - Gol G2 - 2009',
+    },
+    {
+        nome: 'Renault - Logan - 2014',
+    },
+    {
+        nome: 'Toyota - Corolla - 2015',
+    },
+    {
+        nome: 'Fiat - Argo - 2020'
+    },
+    {
+        nome: 'Renault - Clio - 2022'
+    }
+];
+
+window.CARROS_GLOBAL = CARROS_GLOBAL;
+const termoEncontrado = pesquisarCarros(CARROS_GLOBAL, termoCarro);
+console.log('Carros encontrados são: ', termoEncontrado);
--- desafio-refatorado/src/modules/carros.js
+++ desafio-refatorado/src/modules/carros.js
@@ -1,35 +1,17 @@
-// export const pesquisarCarros = (carros, termo) => {
-//     return carros.filter((carro) => {
-//         const nomeMinusculo = carro.nome.toLowerCase();
-//         return nomeMinusculo.includes(termo.toLowerCase())
-//     });
-// }
+export const pesquisarCarros = (carros, termo) => {
+    console.log(CARROS_GLOBAL);
+    return carros.filter((carro) => {
+        const nomeMinusculo = carro.nome;
+        const termoMinusculo = termo.toLowerCase();
+        return nomeMinusculo.includes(termoMinusculo);
+    });
+}
 
-export const pesquisarCarros = (carros, termo) => carros
-    .filter(carro => 
-        carro.nome
-            .toLowerCase()
-            .includes(termo.toLowerCase()));
+// export const pesquisarCarros = (carros, termo) => carros
+//     .filter(carro => 
+//         carro.nome
+//             .toLowerCase()
+//             .includes(termo.toLowerCase()));
 
 
-export const carros = [
-    {
-        nome: 'Fiat - Uno - 2012',
-    },
-    {
-        nome: 'Volkswagen - Gol G2 - 2009',
-    },
-    {
-        nome: 'Renault - Logan - 2014',
-    },
-    {
-        nome: 'Toyota - Corolla - 2015',
-    },
-    {
-        nome: 'Fiat - Argo - 2020'
-    },
-    {
-        nome: 'Renault - Clio - 2022'
-    }
-];
     
